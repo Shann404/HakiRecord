aeb083910bccdacb2a92e7a0c55e87385d074c38
Shann404 <shannbahati@gmail.com>
2025-10-17 11:14:31 +0300

    change debug to false in settings.py

--- HakiRecord/settings.py
+++ HakiRecord/settings.py
@@ -127,7 +127,6 @@ STATICFILES_DIRS = [
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
